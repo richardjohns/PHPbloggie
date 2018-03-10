17794a2c7725a0a82ec3859f884a60e7a480afd9
richardjohns <richard.johns193@gmail.com>
2018-03-10 16:19:52 +1300

    first take at links and _GET

---
+++ second_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Second page</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+    <?php echo "Welcome to second page" ?>
+
+    <?php $link_name = "Home page" ?>
+    <br/>
+    <br/>
+
+    <pre>
+      <?php
+        echo printr($_GET)
+
+        $id = $_GET['id']
+        echo $id
+      ?>
+    </pre>
+
+
+    <a href="welcome.php"><?php echo $link_name ?></a>
+
+    </body>
+</html>
