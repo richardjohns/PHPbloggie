ebed91f929f91563bd92db7e6b960e8329f3dab3
richardjohns <richard.johns193@gmail.com>
2018-03-12 16:04:24 +1300

    Mysql string escape stuff

--- dbinsert.php
+++ dbinsert.php
@@ -20,6 +20,9 @@
 	$position = 4;
 	$visible = 1;
 
+  // Escape all strings
+  $menu_name = mysqli_real_escape_string($connection, $menu_name);
+
 	// 2. Perform database query
 	$query  = "INSERT INTO subjects (";
 	$query .= "  menu_name, position, visible";
